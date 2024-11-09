405ac8a3dc2638d1d5a953cf67d56ec19639efd7
RishabhDoshi98 <rdoshi13@asu.edu>
2024-11-08 19:42:34 -0700

    Made some basic pages - Home page, Cloud Computing and Containers.

--- pns-deployment/src/App.js
+++ pns-deployment/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import CloudComputing from './CloudComputing';
+import Containers from './Containers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Performant and Scalable Deployment Project</h1>
+          
+          <section className="Table-of-contents">
+            <h2>Table of Contents</h2>
+            <ul>
+              <li><Link to="/cloud-computing">Cloud Computing</Link></li>
+              <li><Link to="/containers">Containers</Link></li>
+              <li><a href="#kubernetes">Kubernetes</a></li>
+              <li><a href="#cdn">Content Delivery Networks (CDNs)</a></li>
+              <li><a href="#caching">Caching</a></li>
+              <li><a href="#ci-cd">Continuous Integration and Deployment (CI/CD)</a></li>
+            </ul>
+          </section>
+        </header>
+        <Routes>
+          <Route path="/cloud-computing" element={<CloudComputing />} />
+          <Route path="/containers" element={<Containers />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
